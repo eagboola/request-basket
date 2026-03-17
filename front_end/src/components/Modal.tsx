a88657b4cc79ad3aacd886901182d092edb5eaa5
eagboola <ekerin.m.a@gmail.com>
2026-03-17 18:15:29 -0500

    fix(Modal): add fallback for clipboard copy functionality

--- front_end/src/components/Modal.tsx
+++ front_end/src/components/Modal.tsx
@@ -1,5 +1,6 @@
 import type { BasketUrls } from "../types/BasketUrls";
 import { Link } from 'react-router-dom';
+import copy from 'copy-to-clipboard';
 
 export default function Modal({urls, setVisibleModal}:
   { urls: BasketUrls,
@@ -7,7 +8,17 @@ export default function Modal({urls, setVisibleModal}:
   }) {
 
   function handleCopyURL() {
-    navigator.clipboard.writeText(urls.sendToBasket);
+    // `navigator.clipoard` is a browser API that allows copying text to the clipboard.
+    // It's usually allows in secure contexts - https.
+    // For http, it will be `undefined`, so there needs to be a fallback.
+    // We could use a conditional fallback where if the context is http, we use a deprecated approach to
+    // allow copying the URL, but this isn't best practice.
+    // For deployment, we'd want to either:
+    // - use a registered domain with https
+    // - use an npm library like `copy-to-clipboard` or `clipboard-copy`
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(urls.sendToBasket);
+    } else {}
   }
 
   const basketName = urls.sendToBasket.split('/').slice(-1)[0];
